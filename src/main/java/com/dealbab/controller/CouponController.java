9cd362348a3a1529c3ac43ad453c5df3deea9b0f
sudheerpotti <79624862+sudheerpotti@users.noreply.github.com>
2021-07-04 21:45:26 +0530

    Add files via upload

--- src/main/java/com/dealbab/controller/CouponController.java
+++ src/main/java/com/dealbab/controller/CouponController.java
@@ -1,25 +1,17 @@
 package com.dealbab.controller;
-import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import com.dealbab.exception.CouponAlreadyExistsException;
-import com.dealbab.exception.CouponNotFoundException;
 import com.dealbab.model.Coupon;
 import com.dealbab.repository.CouponRepository;
 import com.dealbab.service.CouponService;
@@ -33,9 +25,13 @@ public class CouponController {
 	private CouponService couponService;
 	 @Autowired()
 	 private CouponRepository couponRepository;
-	 @CrossOrigin(origins = "http://localhost:8080")
+	 
+	 @CrossOrigin(origins = "http://localhost:8090")
 	@PostMapping(value="/coupon")
-	public ResponseEntity<?> createCoupon(@Validated @RequestBody Coupon coupon, UriComponentsBuilder ucBuilder){
+		public Coupon createCoupon(@RequestBody Coupon coupon){
+		 return couponService.createCoupon(coupon);
+			}
+	/*public ResponseEntity<?> createCoupon(@Validated @RequestBody Coupon coupon, UriComponentsBuilder ucBuilder){
 
 		  if (couponRepository.findById(coupon.getId()).isPresent()) {
 		        throw new CouponAlreadyExistsException(coupon.getId());
@@ -46,33 +42,44 @@ public class CouponController {
 		    headers.setLocation(ucBuilder.path("/api/coupon/{id}").buildAndExpand(coupon.getId()).toUri());
 		    return new ResponseEntity<>(headers, HttpStatus.CREATED);
 		}
-	
-/*	@CrossOrigin(origins = "http://localhost:8080")
-	 @GetMapping(value="/coupon/{id}")
-	public Optional<Coupon> checkCouponById(@PathVariable("id")Integer id)
-	{ 
-	return couponService.checkCouponById(id);
-	}
 	*/
-	@CrossOrigin(origins = "http://localhost:8080")
+/*@CrossOrigin(origins = "http://localhost:8090")
+	 @GetMapping(value="/coupon")
+	public Optional<Coupon> checkCouponBySid(@RequestParam("sid")Integer sid)
+	{ 
+	return couponService.checkCouponBySid(sid);
+	}*/
+	
+	/*@CrossOrigin(origins = "http://localhost:8090")
 	@GetMapping(value="/coupon/{id}")
 	public ResponseEntity<Coupon> getCoupon(@PathVariable("id") Integer id) {
 	    return couponRepository.findById(id)
 	        .map(coupon -> new ResponseEntity<>(coupon, HttpStatus.OK))
 	        .orElseThrow(() -> new CouponNotFoundException(id));
-	}
-
-
+	}*/
 	
-	/*	@GetMapping(value="/coupon")
-		public Iterable<Coupon> getAllCoupon()
+		@GetMapping(value="/coupon")
+		public List<Coupon> getAllCoupon(@RequestParam(value="storeId",required = false)String storeId/*,
+				@RequestParam(value="categoryId",required = false)String categoryId*/)
 	 {
-		  return couponService.getAllCoupon();
+		
+		  List<Coupon> couponList= couponService.getAllCoupon();
+		  if(storeId!=null && !storeId.isEmpty()) {
+			
+			  return couponList.stream().filter(c->c.getSid().equalsIgnoreCase(storeId)).collect(Collectors.toList()); 
+		  }
+		 /* else if(categoryId!=null && !categoryId.isEmpty()) {
+			
+			  return couponList.stream().filter(c->c.getCid().equalsIgnoreCase(categoryId)).collect(Collectors.toList()); 
+		  }*/
+		  else {
+			  return couponList;
+		  }
 		
 	 }
-	 */	   
+	 	   
 	
-	 @CrossOrigin(origins = "http://localhost:8080")
+	/* @CrossOrigin(origins = "http://localhost:8090")
 		@GetMapping(value="/coupon")
 		public ResponseEntity<?> coupon() {
 		    List<Coupon> coupon = couponService.getAllCoupon();
@@ -85,9 +92,9 @@ public class CouponController {
 		    hmap.put("coupon", coupon);
 		    return new ResponseEntity<HashMap<String, Object>>(hmap, HttpStatus.OK);
 		}
-	 
+	 */
 	/*@GetMapping("/coupon")
-	 *  http://localhost:8080/dealbab/coupon?page=0&size=1
+	 *  http://localhost:8090/dealbab/coupon?page=0&size=1
 	public List<Coupon> coupon(@Param(value = "page") int page, 
 													@Param(value = "size") int size){
 		Pageable requestedPage = PageRequest.of(page, size);
@@ -95,15 +102,17 @@ public class CouponController {
 		return coupon.toList();
 	}
 	 */
+	
+	 	 
 	 /*
-	 @CrossOrigin(origins = "http://localhost:8080")
+	 @CrossOrigin(origins = "http://localhost:8090")
 	@DeleteMapping(value="/coupon/{id}")
 	public void deleteCoupon(@PathVariable("id")Integer id){
 		couponService.deleteCoupon(id);
 }
 	
 	 */
-	 @CrossOrigin(origins = "http://localhost:8080")
+	 /*@CrossOrigin(origins = "http://localhost:8090")
 	 @DeleteMapping(value="/coupon/{id}")
 	 public ResponseEntity<?> deleteCoupon(@PathVariable("id") Integer id) {
 	     return couponRepository.findById(id)
@@ -113,6 +122,7 @@ public class CouponController {
 	          })
 	          .orElseThrow(() -> new CouponNotFoundException(id));
 	 }
+	*/
 	 
 	 }
     
